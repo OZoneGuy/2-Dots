26c7247fc611ddd1d033f769ce80fdf25889dc02
O-Zone-Guy <alkershomar@gmail.com>
2020-04-01 00:15:19 -0400

    feat - Implemented Moves game mode

---
+++ src/me3/a4/StateMoves.java
@@ -0,0 +1,42 @@
+package me3.a4;
+
+public class StateMoves extends GameState {
+    private int moves;
+    private int maxMoves;
+    private int score;
+    private int scoreGoal;
+
+    public StateMoves(int score, int moves){
+        super();
+        this.score     = 0;
+        this.moves     = 0;
+        this.maxMoves  = moves;
+        this.scoreGoal = score;
+    }
+
+    public void update(){
+        return;
+    }
+
+    public void update(int n, BoardT.Colour c){
+        score += super.calcScore(n, c);
+        moves++;
+        if (moves >= maxMoves)
+            if (score >= scoreGoal)
+                state = GameState.State.WIN;
+            else
+                state = GameState.State.LOSE;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public int getScoreGoal(){
+        return scoreGoal;
+    }
+
+    public int getRemMoves(){
+        return maxMoves - moves;
+    }
+}
