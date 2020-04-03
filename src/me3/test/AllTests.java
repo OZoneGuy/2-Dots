be33ce0987cc128abdfbe3475c0a5a091987bfd1
O-Zone-Guy <alkershomar@gmail.com>
2020-04-02 20:32:25 -0400

    test - Added a runner class

---
+++ src/me3/test/AllTests.java
@@ -0,0 +1,13 @@
+package me3.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestBoardT.class,
+            TestGameState.class,
+
+    })
+
+public class AllTests{}
